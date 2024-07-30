c5ed0d1167a5f8e75bb89b0f0fefdb3c84fdc71b
Satyam <hello@satyvm.com>
2024-07-31 00:20:40 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import * as THREE from 'three'
+import { useRef, useState } from 'react'
+import { Canvas, extend, useThree, useFrame } from '@react-three/fiber'
+import { BallCollider, CuboidCollider, Physics, RigidBody, useRopeJoint, useSphericalJoint } from '@react-three/rapier'
+import { MeshLineGeometry, MeshLineMaterial } from 'meshline'
+
+extend({MeshLineGeometry, MeshLineMaterial})
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Canvas>
+        <Physics>
+
+        </Physics>
+      </Canvas>
+  )
 }
 
-export default App;
+function Band() {
+  const band = useRef()
+  const fixed = useRef()
+  const j1 = useRef()
+  const j2 = useRef()
+  const j3 = useRef()
+
+  // Canvas size
+  const {width, height} = useThree((state) => state.size)
+
+  // A Catmull-Rom curve
+  const [curve] = useState(() => new THREE.CatmullRomCurve3([
+    new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3()
+  ]))
+}
